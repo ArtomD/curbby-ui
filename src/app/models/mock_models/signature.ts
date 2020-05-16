5166cd6d48f36481c44a76f4b33b58df8ccbda22
Artom Didinchuk <didinchukar@gmail.com>
2020-05-16 10:41:51 -0400

    updating sig

--- src/app/models/mock_models/signature.ts
+++ src/app/models/mock_models/signature.ts
@@ -1,9 +1,7 @@
 export const mock_signature = {
-	"signature" : {
-		"hmac": "37b41495d6047d6dcf81b2b9008d9fccbbe260df44d3a495a3af98c8325e8f4b",
-		"locale": "en",
-		"session": "ed6e2b92b68fdd93607f8f54b5649250c7bbb22bcbe2d01b2f7fb2ee472159ce",
-		"shop" : "dev-pencil.myshopify.com",
-		"timestamp": "1589422870"
-	}
+  "hmac": "37b41495d6047d6dcf81b2b9008d9fccbbe260df44d3a495a3af98c8325e8f4b",
+  "locale": "en",
+  "session": "ed6e2b92b68fdd93607f8f54b5649250c7bbb22bcbe2d01b2f7fb2ee472159ce",
+  "shop" : "dev-pencil.myshopify.com",
+  "timestamp": "1589504513"
 };
