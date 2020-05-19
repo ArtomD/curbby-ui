b338e5f9fcf147b2ba1378ca2c8238832a7705f6
Artom Didinchuk <didinchukar@gmail.com>
2020-05-19 00:06:09 -0400

    removing uneeded files

--- src/app/models/countries.ts
+++
@@ -1,4 +0,0 @@
-export const countries = [
-    {name:"Canada", code:"ca", prefix:"+1"},
-    {name:"United States", code:"us", prefix:"+1"},
-]
--- src/app/models/country-code.ts
+++
@@ -1,5 +0,0 @@
-export const countries = [
-    {country: "Canada", code: "+1"},
-    {country: "USA", code: "+1"},
-]
-
