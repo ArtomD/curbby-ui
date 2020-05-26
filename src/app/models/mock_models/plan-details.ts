668f5d891b6b53911871dc83156626a76af5b735
Artom Didinchuk <didinchukar@gmail.com>
2020-05-25 23:50:28 -0400

    updating model

---
+++ src/app/models/mock_models/plan-details.ts
@@ -0,0 +1,44 @@
+export const mock_billing_details = {
+    "plan": [
+        {
+            "id": "3",
+            "fee": 9.99,
+            "created": "2020-05-23T21:52:34.291Z",
+            "modified": "2020-05-23T21:52:34.291Z",
+            "name": "STANDARD",
+            "billable": true,
+            "credits": 300,
+            "standard": true
+        },
+        {
+            "id": "4",
+            "fee": 19.99,
+            "created": "2020-05-23T21:52:55.165Z",
+            "modified": "2020-05-23T21:52:55.165Z",
+            "name": "PROFESSIONAL",
+            "billable": true,
+            "credits": 800,
+            "standard": true
+        },
+        {
+            "id": "5",
+            "fee": 49.99,
+            "created": "2020-05-23T21:53:21.104Z",
+            "modified": "2020-05-23T21:53:21.104Z",
+            "name": "PREMIUM",
+            "billable": true,
+            "credits": 2500,
+            "standard": true
+        },
+        {
+            "id": "2",
+            "fee": 4.99,
+            "created": "2020-05-21T05:07:11.618Z",
+            "modified": "2020-05-21T05:07:11.618Z",
+            "name": "BASIC",
+            "billable": true,
+            "credits": 100,
+            "standard": true
+        }
+    ]
+}
